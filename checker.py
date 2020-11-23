9a27545a459ebf836e81f33872ea372efadd243f
SamPrent <62377582+SamPrent@users.noreply.github.com>
2020-11-23 20:42:48 +1000

    Update checker.py
    cURL bonus round added

--- checker.py
+++ checker.py
@@ -2,13 +2,17 @@ import sys
 import urllib.request
 import sched
 import time
+import json
 try:
     import bs4 as bs
+    import requests
 except SystemError:
-    print('This module requires site packages "BeautifulSoup" and "lxml"')
+    print('This module requires site packages "BeautifulSoup", "lxml" and "requests"')
     print('Install these by using the "pip install beaufitulsoup4" command')
     print('And')
     print('"pip install lxml"')
+    print('And')
+    print('"pip install requests"')
 
 # Notes: Tweet Checker uses urllib, beautiful soup & lmxl site packages
 # Author: Samuel Prentice
@@ -24,12 +28,22 @@ class TweetChecker:
     made_first_request = False
 
     def __init__(self, *args):
-        if len(args[0]) != 2:
+        # Check args provided
+        if len(args[0]) < 2:
             print('checker.py takes 1 additional argument, you provided', str(len(sys.argv)))
-            print(str(args))
-            print(str(len(args)))
-        else:
+        elif '--help' in args[0]:
+            print('TwitterChecker <twitter handle> <option> <option url>')
+            print()
+            print('To just print out latest tweets, just enter a user handle without the @ symbol')
+            print('Most recent tweets will be updated every 10 minutes')
+            print('To use the curl option enter "user_handle -curl website_to_post_to"')
+        elif len(args[0]) == 2:
             self.handle = args[0][1]
+        elif len(args[0]) == 4:
+            if '-curl' == args[0][2]:
+                self.handle = args[0][1]
+                self.run_all()
+                self.post_curl(args[0][3])
 
     def run_all(self):
         self.make_request()
@@ -58,6 +72,13 @@ class TweetChecker:
         for tweet in range(0, 5):
             print(self.collection[tweet])
 
+    def post_curl(self, url):
+        payload = self.get_json()
+        requests.post(url, data=payload)
+
+    def get_json(self):
+        return json.dumps(self.__dict__)
+
 
 def update_every_ten_minutes(checker):
     checker.run_all()
@@ -66,5 +87,12 @@ def update_every_ten_minutes(checker):
 
 
 if __name__ == '__main__':
-    tc = TweetChecker(sys.argv)
-    update_every_ten_minutes(tc)
+    if len(sys.argv) > 1:
+        tc = TweetChecker(sys.argv)
+        update_every_ten_minutes(tc)
+    else:
+        # Option to enter it later if missed in original args
+        print('Welcome to TwitterChecker')
+        handle = input('Enter twitter handle:')
+        tc = TweetChecker(['checker.py', handle])
+        update_every_ten_minutes(tc)
