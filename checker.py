c62a66207541e192f42a583e47900d0fb4f8379d
SamPrent <62377582+SamPrent@users.noreply.github.com>
2020-11-23 18:47:05 +1000

    Update checker.py
    Initial class with working code

--- checker.py
+++ checker.py
@@ -1,39 +1,57 @@
 import sys
 import urllib.request
-import bs4 as bs
+try:
+    import bs4 as bs
+except SystemError:
+    print('This module requires site packages "BeautifulSoup" and "lxml"')
+    print('Install these by using the "pip install beaufitulsoup4" command')
+    print('And')
+    print('"pip install lxml"')
 
-# check tweets
-
-# grab args
-if len(sys.argv) != 2:
-    print('checker.py takes 1 additional argument, you provided', str(len(sys.argv)))
-else:
-    # take in a command line arg for twitter account
-    handle = sys.argv[1]
-
-# build twitter link
-url = 'https://www.twitter.com/' + handle
-
-# get page info
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
-request = urllib.request.Request(url, headers=headers)
-response = urllib.request.urlopen(request)
-
-# Soup the tweets from rest of it
-soup = bs.BeautifulSoup(response, 'lxml')
-tweets = soup.find_all('div', {"class": "tweet-text"})
-#tweet_dates = soup.find_all('time')
-
-# print out 5 most recent tweets to stdout
-print('Printing 5 most recent tweets for @' + handle)
-for t in range(0, 5):
-#    print(tweet_dates[t].text)
-    print(tweets[t].text)
-
-#for t in tweet_dates:
-#    print(t)
-
-# check again for new tweets in 10 min's and print any new ones
+# Notes: Tweet Checker uses urllib, beautiful soup & lmxl site packages
+# Author: Samuel Prentice
+# Last Update: 23-11-2020
 
 
+class TweetChecker:
+    handle = ''
+    url = 'https://www.twitter.com/'
+    collection = []
+    made_first_request = False
 
+    def __init__(self, *args):
+        if len(args[0]) != 2:
+            print('checker.py takes 1 additional argument, you provided', str(len(sys.argv)))
+            print(str(args))
+            print(str(len(args)))
+        else:
+            self.handle = args[0][1]
+            self.make_request()
+            self.make_soup()
+
+    def make_request(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) '
+                                 'Chrome/24.0.1312.27 Safari/537.17'}
+        request = urllib.request.Request(self.url + self.handle, headers=headers)
+        return request
+
+    def make_soup(self):
+        response = urllib.request.urlopen(self.make_request())
+        soup = bs.BeautifulSoup(response, 'lxml')
+        tweets = soup.find_all('div', {"class": "tweet-text"})
+        for tweet in tweets:
+            if tweet.text not in self.collection:
+                if self.made_first_request:
+                    self.collection.insert(0, tweet.text)
+                else:
+                    self.collection.append(tweet.text)
+        self.made_first_request = True
+
+    def print_last_five(self):
+        for tweet in range(0, 5):
+            print(self.collection[tweet])
+
+
+if __name__ == '__main__':
+    tc = TweetChecker(sys.argv)
+    tc.print_last_five()
